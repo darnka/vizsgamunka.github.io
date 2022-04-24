d0f3d8e84fbaed03e5045a6896785bd2ddd578c9
Both Andrea <94750813+darnka@users.noreply.github.com>
2022-04-24 23:24:57 +0200

    Add files via upload

---
+++ kezdolap.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Pávakert Lakóközösség</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.png" />
+  </head>
+
+  <body>
+    
+
+  <div class="elmosodott-image"></div>
+    <div class="elmosodott-text text-center">
+        <img src="img/logo.png" alt="logo"> 
+       <h1 class="display-1 pb-5">PÁvaKert</h1>
+       <h1 class=" pb-5">Üdvözöljük Lakóközösségünk Weboldalán!</h1>
+       <div class="py-2">
+           <a href="bejelentkezes.php" type="button" class="btn btn-light text-uppercase">Belépek</a>
+       </div>
+    </div>
+
+  </body>
+
+
+
+</html>
